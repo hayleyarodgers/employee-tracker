ce6e2cf93a74a90bf6f0840fb92c83296beafbaf
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-08-05 13:52:41 +1000

    Progress update on view all employees query

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ const respondToUserInputs = () => {
 
     // View all employees
     const viewAllEmployees = () => {
-        const sql = `SELECT employees.id AS id, employees.first_name AS first_name, employees.last_name AS last_name, roles.title AS title, departments.name AS department, roles.salary AS salary, employees.manager_id AS manager FROM employees JOIN roles ON employees.role_id = roles.id JOIN departments ON roles.department_id = departments.id;`;
-        
+        const sql = `SELECT e.id AS id, e.first_name AS first_name, e.last_name AS last_name, roles.title AS title, departments.name AS department, roles.salary AS salary, CONCAT(m.first_name, " ", m.last_name) AS manager FROM employees e JOIN roles ON e.role_id = roles.id JOIN departments ON roles.department_id = departments.id JOIN employees m ON e.manager_id = m.id`;
+
         db.query(sql, (err, result) => {
             if (err) {
                 console.log(err);
