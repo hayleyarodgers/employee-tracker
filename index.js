c82f531146f5d2ab9f9d3eba1c74794c6a50aae1
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-08-06 11:08:27 +1000

    Finish writing helper function for getting a list of departments and making edits to addRole

--- index.js
+++ index.js
@@ -92,59 +92,50 @@ const respondToUserInputs = () => {
     }
     
     // Add a role
-    const addRole = () => {        
-        db.query(`SELECT name FROM departments;`, (err, result) => {
-            if (err) {
-                console.log(err);
-            };
+    const addRole = async () => {        
+        const departmentChoices = await getListofCurrentDepartments();
 
-            const departmentChoices = [];
-            for (let i = 0; i < result.length; i++) {
-                departmentChoices.push(result[i].name);
-            };
-
-            inquirer
-            .prompt([
-                {
-                    type: 'input',
-                    name: 'title',
-                    message: 'What is the title of the role?'
-                },
-                {
-                    type: 'input',
-                    name: 'salary',
-                    message: 'What is the salary of the role?'
-                },
-                {
-                    type: 'list',
-                    name: 'department',
-                    message: 'Which department does the role belong to?',
-                    choices: departmentChoices
-                }
-            ])
-            .then((answers) => {
-                let chosenSalary = parseInt(answers.salary);
-                
-                let chosenDepartment = answers.department;
-                let chosenDepartmentID;
-                for (let i = 0; i < departmentChoices.length; i++) {
-                    if (chosenDepartment = departmentChoices[i]) {
-                        chosenDepartmentID = i + 1;
-                    };
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the title of the role?'
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'What is the salary of the role?'
+            },
+            {
+                type: 'list',
+                name: 'department',
+                message: 'Which department does the role belong to?',
+                choices: departmentChoices
+            }
+        ])
+        .then((answers) => {
+            const chosenSalary = parseInt(answers.salary);
+            
+            const chosenDepartment = answers.department;
+            let chosenDepartmentID;
+            for (let i = 0; i < departmentChoices.length; i++) {
+                if (chosenDepartment == departmentChoices[i]) {
+                    chosenDepartmentID = i + 1;
                 };
+            };
 
-                const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?)`;
-                const params = [answers.title, chosenSalary, chosenDepartmentID];
-                
-                db.query(sql, [params], (err, result) => {
-                    if (err) {
-                        console.log(err);
-                    }
-        
-                    decisionInput();
-                });
+            const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?)`;
+            const params = [answers.title, chosenSalary, chosenDepartmentID];
+            
+            db.query(sql, [params], (err, result) => {
+                if (err) {
+                    console.log(err);
+                }
+    
+                decisionInput();
             });
-        })
+        });
     }
 
     // Add an employee
@@ -208,55 +199,65 @@ const respondToUserInputs = () => {
         });
     }
 
-        /* DECISION */
-        // User says what they want to do
-        const decisionInput = () => {
-            inquirer
-            .prompt([
-                {
-                    type: 'list',
-                    name: 'decision',
-                    message: 'What would you like to do?',
-                    choices: ['View all departments',
-                              'View all roles',
-                              'View all employees',
-                              'Add a department',
-                              'Add a role',
-                              'Add an employee',
-                              'Update an employee\'s role',
-                              'Quit'],
-                }
-            ])
-            .then((answers) => {        
-                if (answers.decision === 'View all departments') {
-                    viewAllDepartments();
-                } else if (answers.decision === 'View all roles') {
-                    viewAllRoles();
-                } else if (answers.decision === 'View all employees') {
-                    viewAllEmployees();
-                } else if (answers.decision === 'Add a department') {
-                    addDepartment();
-                } else if (answers.decision === 'Add a role') {
-                    addRole();
-                } else if (answers.decision === 'Add an employee') {
-                    addEmployee();
-                } else if (answers.decision === 'Update an employee\'s role') {
-                    updateEmployeeRole();
-                } else {
-                    console.log('Thanks for using the employee tracker, bye! 👋');
-                    return
-                }
-            });
-        }
-    
-        decisionInput();
+    /* DECISION */
+    // User says what they want to do
+    const decisionInput = () => {
+        inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'decision',
+                message: 'What would you like to do?',
+                choices: ['View all departments',
+                            'View all roles',
+                            'View all employees',
+                            'Add a department',
+                            'Add a role',
+                            'Add an employee',
+                            'Update an employee\'s role',
+                            'Quit'],
+            }
+        ])
+        .then((answers) => {        
+            if (answers.decision === 'View all departments') {
+                viewAllDepartments();
+            } else if (answers.decision === 'View all roles') {
+                viewAllRoles();
+            } else if (answers.decision === 'View all employees') {
+                viewAllEmployees();
+            } else if (answers.decision === 'Add a department') {
+                addDepartment();
+            } else if (answers.decision === 'Add a role') {
+                addRole();
+            } else if (answers.decision === 'Add an employee') {
+                addEmployee();
+            } else if (answers.decision === 'Update an employee\'s role') {
+                updateEmployeeRole();
+            } else {
+                console.log('Thanks for using the employee tracker, bye! 👋');
+                return
+            }
+        });
+    }
+
+    decisionInput();
 }
 
 // Getting list of current departments
+const getListofCurrentDepartments = async () => {  
+    const data = await db.promise().query(`SELECT name FROM departments;`);
+    const departmentObject = data[0];
 
+    const departmentChoices = [];
+
+    for (let i = 0; i < departmentObject.length; i++) {
+        departmentChoices.push(departmentObject[i].name);
+    };
+
+    return departmentChoices;
+};
 
 // Getting list of current roles
-
 
 // Getting list of current employees
 
